e89ee6d577b8a810c63d3942931278eb486e60bb
@josetrujisilv.disenio <josetrujisilv.disenio@gmail.com>
2025-04-28 10:48:17 +0200

    cambios 28-04-2025

--- resources/js/views/auth/Register.vue
+++ resources/js/views/auth/Register.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
   import '../../../css/style.css';
+  import FormReg from '../components/widgets/FormReg.vue';
 </script>
 
 <template>
-  <div class="fond-img-login">
-    <h1 style="color: #fff;">Hola, estas en registro</h1>
+  <div class="fond-img-reg">
+    <div class="content-reg row flex-column justify-center align-center">
+      <FormReg/>
+    </div>
   </div>
 </template>
--- resources/js/views/components/widgets/Logos/Logo_SoundWave.vue
+++ resources/js/views/components/widgets/Logos/Logo_SoundWave.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="d-flex justify-center align-center g-1">
-        <img width="80" src="@/../img/svg/soundwave-logo-blanco.svg" alt="">
+        <img width="70" src="@/../img/svg/soundwave-logo-blanco.svg" alt="">
         <p class="text-logo">Soundwave</p>
   </div>
 </template>
